c231012cfd696cf4f5ccc5ef90e8faaebc743e64
unknown <dgray.stlouis@gmail.com>
2022-03-19 19:03:31 -0500

    lesson1 part1 commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flint_Rasmussen.jpg/220px-Flint_Rasmussen.jpg';
+  image3 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flint_Rasmussen.jpg/220px-Flint_Rasmussen.jpg';
 
   constructor() { }
 
